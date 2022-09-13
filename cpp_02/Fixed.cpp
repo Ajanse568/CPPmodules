e6deec941e24ed36006bfddd112df3ad9a505002
Alle Janse <ajanse97@gmail.com>
2022-09-13 13:56:48 +0200

    cpp02

---
+++ cpp_02/Fixed.cpp
@@ -0,0 +1,20 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() {
+
+	std::cout << "Default constructor called" << std::endl;
+	th
+}
+
+Fixed::~Fixed( void ) {
+
+}
+
+int	Fixed::getRawBits( void ) const {
+
+}
+
+void	Fixed::setRawBits(int const raw) {
+
+}
